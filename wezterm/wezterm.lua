13202499e9e76c5abcfed48bfa1979e2aad461ca
oliwia <24637207+kkanden@users.noreply.github.com>
2025-05-29 19:15:52 +0200

    batch commit

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,16 +3,17 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 -- FONT
+local harfbuzz = { "zero", "calt" }
 local default_font = {
     family = "JetBrainsMono Nerd Font",
     weight = "Medium",
-    harfbuzz_features = {},
+    harfbuzz_features = harfbuzz,
 }
 local default_font_italic = {
     family = "JetBrainsMono Nerd Font",
     weight = "Medium",
     italic = true,
-    harfbuzz_features = { "zero" },
+    harfbuzz_features = harfbuzz,
 }
 
 config.font = wezterm.font(default_font)
@@ -35,7 +36,7 @@ config.window_padding = {
 }
 
 config.color_schemes = {
-    ["kanagawa"] = {
+    kanagawa = {
         foreground = "#dcd7ba",
         background = "#1f1f28",
         cursor_bg = "#c8c093",
@@ -49,21 +50,32 @@ config.color_schemes = {
         brights = { "#727169", "#e82424", "#98bb6c", "#e6c384", "#7fb4ca", "#938aa9", "#7aa89f", "#dcd7ba" },
         indexed = { [16] = "#ffa066", [17] = "#ff5d62" },
     },
+    gruvbox_material_dark_medium = {
+        foreground = "#D4BE98",
+        background = "#282828",
+        cursor_bg = "#D4BE98",
+        cursor_border = "#D4BE98",
+        cursor_fg = "#282828",
+        selection_bg = "#D4BE98",
+        selection_fg = "#45403d",
+        ansi = { "#282828", "#ea6962", "#a9b665", "#d8a657", "#7daea3", "#d3869b", "#89b482", "#d4be98" },
+        brights = { "#eddeb5", "#ea6962", "#a9b665", "#d8a657", "#7daea3", "#d3869b", "#89b482", "#d4be98" },
+    },
 }
 
-config.color_scheme = "kanagawa"
+config.color_scheme = "gruvbox_material_dark_medium"
 config.force_reverse_video_cursor = true
 
 config.initial_rows = 24
 config.initial_cols = 80
 config.window_background_opacity = 0.75
-config.win32_system_backdrop = "Acrylic"
+-- config.win32_system_backdrop = "Acrylic"
 
 -- KEYBINDINGS
 config.disable_default_key_bindings = true
 config.keys = {
-    { key = "c", mods = "CTRL|SHIFT", action = wezterm.action.CopyTo("Clipboard") },
     { key = "v", mods = "CTRL|SHIFT", action = wezterm.action.PasteFrom("Clipboard") },
+    { key = "c", mods = "CTRL|SHIFT", action = wezterm.action.CopyTo("Clipboard") },
     { key = "t", mods = "CTRL|SHIFT", action = wezterm.action.SpawnTab("CurrentPaneDomain") },
     { key = "w", mods = "CTRL|SHIFT", action = wezterm.action.CloseCurrentTab({ confirm = false }) },
     { key = "RightArrow", mods = "CTRL|SHIFT", action = wezterm.action.ActivateTabRelative(1) },
@@ -75,6 +87,7 @@ config.keys = {
 }
 
 -- GENERAL TERMINAL SETTINGS
+config.automatically_reload_config = true
 config.max_fps = 120
 config.show_tabs_in_tab_bar = true
 config.window_decorations = "RESIZE"
